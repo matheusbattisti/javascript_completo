6d7251da2cf3690cb15266e724fb3e0ad5ce1fd7
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-10-31 20:35:09 -0300

    continuando oop

--- 6_OOP/js/scripts.js
+++ 6_OOP/js/scripts.js
@@ -28,3 +28,95 @@ console.log(pessoa.getNome());
 pessoa.setNome("Matheus Battisti");
 
 console.log(pessoa.getNome());
+
+// 3 - prototype
+const text = "asd";
+
+console.log(Object.getPrototypeOf(text));
+
+const bool = true;
+
+console.log(Object.getPrototypeOf(bool));
+
+const arr = [];
+
+console.log(arr.length);
+
+console.log(Object.getPrototypeOf(arr) === Array.prototype);
+
+// 4 - mais sobre prototype
+const myObject = {
+  a: "b",
+};
+
+console.log(Object.getPrototypeOf(myObject));
+
+console.log(Object.getPrototypeOf(myObject) === Object.prototype);
+
+const mySecondObject = Object.create(myObject);
+
+console.log(mySecondObject);
+
+console.log(Object.getPrototypeOf(mySecondObject) === myObject);
+
+// 5 - classes básicas
+const cachorro = {
+  raca: null,
+};
+
+const pastorAlemao = Object.create(cachorro);
+
+pastorAlemao.raca = "Pastor Alemão";
+
+const bulldog = Object.create(cachorro);
+
+bulldog.raca = "Bulldog";
+
+// 6 - funcoes como classes
+function criarCachorro(nome, raca) {
+  const cachorro = Object.create({});
+  cachorro.raca = raca;
+  cachorro.nome = nome;
+  return cachorro;
+}
+
+const bob = criarCachorro("Bob", "Vira lata");
+
+console.log(bob);
+
+const jack = criarCachorro("Jack", "Poodle");
+
+console.log(jack);
+
+console.log(Object.getPrototypeOf(jack));
+
+// 7 - funcoes construtoras
+function Cachorro(nome, raca) {
+  this.nome = nome;
+  this.raca = raca;
+}
+
+const husky = new Cachorro("Ozzy", "Husky");
+
+console.log(husky);
+
+// 8 - métodos na função construtora
+Cachorro.prototype.uivar = function () {
+  console.log("Auuu");
+};
+
+husky.uivar();
+
+// 9 - classes es6
+class CachorroClasse {
+  constructor(nome, raca) {
+    this.nome = nome;
+    this.raca = raca;
+  }
+}
+
+const jeff = new CachorroClasse("Jeff", "Labrador");
+
+console.log(jeff);
+
+console.log(Object.getPrototypeOf(jeff));
