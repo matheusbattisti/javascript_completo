9a883589db2a961e0f375949e07ae6877c160a2d
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-11-10 23:19:24 -0300

    iniciando DOM

---
+++ 9_DOM/js/scripts.js
@@ -0,0 +1,10 @@
+// 1 - movendo-se no DOM
+console.log(document.body);
+
+console.log(document.body.childNodes);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1].textContent);
